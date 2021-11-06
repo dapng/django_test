a569e3ee74ec885fc59f738e27c2a0d98727069f
Данила Богославцев <da.png@yandex.ru>
2021-11-06 11:02:25 +0300

    feat: add static file and news model

--- one/main/views.py
+++ one/main/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 20,
+            'hobby': 'OSINT'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
