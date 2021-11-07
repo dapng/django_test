cb82b0edf1bcf24fabf3cfb2c42cbc4fb58c48dc
Данила Богославцев <da.png@yandex.ru>
2021-11-07 23:27:55 +0300

    feat: add post method for news

---
+++ one/news/forms.py
@@ -0,0 +1,28 @@
+from django.db import models
+from .models import Articles
+from django.forms import ModelForm, fields, widgets, TextInput, DateTimeInput, Textarea
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anons', 'full_text', 'date']
+
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название статьи'
+            }),
+            "anons": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            "date": DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Дата публикации'
+            }),
+            "full_text": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст статьи'
+            }),
+        }
--- one/news/views.py
+++ one/news/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 # Create your views here.
 
 def news_home(request):
-    news = Articles.objects.all()
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма не верна'
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        # 'error': error,
+    }
+    return render(request, 'news/news_create.html', data)
