0b62117009ee5184748bdf2369c40a8f225ea181
Данила Богославцев <da.png@yandex.ru>
2021-11-24 19:17:46 +0300

    fead: adding, changing, viewing, deleting articles

--- one/news/views.py
+++ one/news/views.py
@@ -1,7 +1,10 @@
-from django import forms
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+
+from . import models
+# from one.news import models
 
 # Create your views here.
 
@@ -10,12 +13,30 @@ def news_home(request):
     return render(request, 'news/news_home.html', {'news': news})
 
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/news_create.html'
+
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news_delete.html'
+
+
 def create(request):
     if request.method == 'POST':
         form = ArticlesForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('.')
         else:
             error = 'Форма не верна'
 
